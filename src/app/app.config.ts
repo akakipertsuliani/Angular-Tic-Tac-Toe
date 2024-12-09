7a88e3227a070bcb7af1883c5ca927261499923e
akakipertsuliani <akaki@pertsuliani>
2024-12-09 22:54:52 +0400

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,27 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+import { Environment } from '../../env';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: Environment.firebase.projectId,
+        appId: Environment.firebase.appId,
+        storageBucket: Environment.firebase.storageBucket,
+        apiKey: Environment.firebase.apiKey,
+        authDomain: Environment.firebase.authDomain,
+        messagingSenderId: Environment.firebase.messagingSenderId,
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
